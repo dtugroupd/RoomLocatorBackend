ae2dedb13175702e4f1ae6a0a2db5d147e7243f6
awo <awo@it-minds.dk>
2019-09-13 13:28:31 +0200

    Setup index controller

---
+++ RoomLocator/RoomLocator.Api/Controllers/IndexController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace RoomLocator.Api.Controllers
+{
+    [Route("/")]
+    public class IndexController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Index()
+        {
+            return Ok("This pod is healthy");
+        }
+    }
+}
