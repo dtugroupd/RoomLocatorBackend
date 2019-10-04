c125e548247a56619f1cc21286c50d0adee1ce0f
Gaurav Dang <gaurav.dang@outlook.com>
2019-10-04 19:21:21 +0200

    Created "MazeMapController" & "MazeMapCoordinatesViewModel"

---
+++ RoomLocator/RoomLocator.Api/Controllers/MazeMapController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RoomLocator.Domain.ViewModels;
+
+namespace RoomLocator.Api.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class MazeMapController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<MazeMapCoordinatesViewModel> GetStaticPoint()
+        {
+            return Ok(new MazeMapCoordinatesViewModel { Latitude = 55.78498471097425, Longitude = 12.52026114549633 });
+        }
+
+    }
+}
