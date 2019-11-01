470978ad1e3c8a7d29b5590b8df34b7531f333d6
tlien <thomaslienchristensen@gmail.com>
2019-11-01 16:39:10 +0100

    Add comment to Survey.

--- RoomLocator/RoomLocator.Domain/Models/Survey.cs
+++ RoomLocator/RoomLocator.Domain/Models/Survey.cs
@@ -7,8 +7,9 @@ namespace RoomLocator.Domain.Models
     public class Survey
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string Comment { get; set; }
         public string Description { get; set; }
+        public string Title { get; set; }
         public DateTime CreatedDate { get; set; }
         public virtual IEnumerable<Question> Questions { get; set; }
         public virtual IEnumerable<MazeMapSection> MazeMapSections { get; set; }
