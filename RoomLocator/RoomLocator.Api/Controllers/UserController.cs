8018e0e83e27165a6284256c67db3b8cab64c85e
algorythm <anders@wiberg.tech>
2019-11-04 20:08:14 +0100

    Fix errors in user controller

--- RoomLocator/RoomLocator.Api/Controllers/UserController.cs
+++ RoomLocator/RoomLocator.Api/Controllers/UserController.cs
@@ -11,6 +11,7 @@ using RoomLocator.Domain.ViewModels;
 namespace RoomLocator.Api.Controllers
 {
     [ApiVersion("1.0")]
+    [Authorize]
     [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
@@ -22,8 +23,17 @@ namespace RoomLocator.Api.Controllers
             _userService = userService;
         }
 
+        [HttpGet("me")]
+        public async Task<ActionResult<UserViewModel>> GetCurrentUser()
+        {
+            var userId = User.Claims.FirstOrDefault(x => x.Type == "uid")?.Value;
+            var user = await _userService.Get(userId);
+
+            return user;
+        }
+
         [HttpGet]
-        [Authorize(Roles = "User")]
+        [Authorize(Roles = "admin")]
         public async Task<ActionResult<IEnumerable<UserViewModel>>> Get()
         {
             var users = await _userService.Get();
@@ -31,12 +41,14 @@ namespace RoomLocator.Api.Controllers
         }
 
         [HttpGet("{id}")]
+        [Authorize(Roles = "admin")]
         public async Task<ActionResult<UserViewModel>> Get(string studentId)
         {
-            return Ok(await _userService.Get(studentId));
+            return Ok(await _userService.GetByStudentId(studentId));
         }
 
         [HttpDelete("id")]
+        [Authorize(Roles = "admin")]
         public async Task<ActionResult> Delete(string studentId)
         {
             await _userService.Delete(studentId);
