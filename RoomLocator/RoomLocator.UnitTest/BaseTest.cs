597dfe12363c8a62e90b0cc2d02c3525a9f522d8
Anders Wiberg Olsen <awo@it-minds.dk>
2019-11-14 23:20:13 +0100

    Test Create in UserService

---
+++ RoomLocator/RoomLocator.UnitTest/BaseTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
+using Moq;
+using RoomLocator.Data.Config;
+using RoomLocator.Domain.Config;
+
+namespace RoomLocator.UnitTest
+{
+    public class BaseTest
+    {
+        protected readonly DbContextOptions<RoomLocatorContext> Options;
+        protected readonly IMapper Mapper;
+        protected ILogger<T> Logger<T>() => new Mock<ILogger<T>>().Object;
+
+        public BaseTest()
+        {
+            Options = new DbContextOptionsBuilder<RoomLocatorContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+                .Options;
+            Mapper = AutoMapperConfig.CreateMapper();
+        }
+    }
+}
---
+++ RoomLocator/RoomLocator.UnitTest/Services/UserServiceTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RoomLocator.Data.Config;
+using RoomLocator.Data.Services;
+using RoomLocator.Domain;
+using RoomLocator.Domain.Models;
+using Xunit;
+
+namespace RoomLocator.UnitTest.Services
+{
+    public class UserServiceTests : BaseTest
+    {
+        [Fact]
+        public async Task Create_CreatesTheUser_AssignsStudentRole()
+        {
+            var studentId = "s123456";
+            var expectedRole = "student";
+
+            using (var context = new RoomLocatorContext(Options))
+            {
+                await context.Roles.AddAsync(new Role {Name = expectedRole});
+                await context.SaveChangesAsync();
+
+                var userService = new UserService(context, Mapper);
+                await userService.Create(studentId);
+            }
+
+            using (var context = new RoomLocatorContext(Options))
+            {
+                var user = await context.Users
+                    .Where(x => x.StudentId == studentId)
+                    .Include(x => x.UserRoles)
+                    .ThenInclude(x => x.Role)
+                    .FirstOrDefaultAsync();
+                
+                Assert.NotNull(user);
+                Assert.Equal(user.StudentId, studentId);
+
+                var hasRole = user.UserRoles.Any(ur => ur.Role.Name.ToLower() == expectedRole);
+                Assert.True(hasRole);
+            }
+        }
+    }
+}
