2832eb7879be23e65722a8e38bbfb3369209aa78
Anders Wiberg Olsen <awo@it-minds.dk>
2019-11-15 02:59:49 +0100

    Initial setup for integration testing controllers

---
+++ RoomLocator/RoomLocator.UnitTest/IntegrationTests/ControllerTests/UserControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace RoomLocator.UnitTest.IntegrationTests.ControllerTests
+{
+    public class UserControllerTests : IClassFixture<RoomLocatorApplicationFactory<Api.Startup>>
+    {
+        private readonly HttpClient _client;
+        private readonly RoomLocatorApplicationFactory<Api.Startup> _factory;
+
+        public UserControllerTests(RoomLocatorApplicationFactory<Api.Startup> factory)
+        {
+            _factory = factory;
+            _client = _factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+        }
+
+        [Fact]
+        public async Task GetUserMe()
+        {
+            var page = await _client.GetAsync("/me");
+            var content = await page.Content.ReadAsStringAsync();
+            
+            Console.WriteLine("stuff");
+        }
+    }
+}
---
+++ RoomLocator/RoomLocator.UnitTest/IntegrationTests/RoomLocatorApplicationFactory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RoomLocator.Api.Helpers;
+using RoomLocator.Data.Config;
+
+namespace RoomLocator.UnitTest.IntegrationTests
+{
+    public class RoomLocatorApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(RoomLocatorContext));
+
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                services.AddDbContext<RoomLocatorContext>((options, context) =>
+                {
+                    context.UseInMemoryDatabase(Guid.NewGuid().ToString());
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<RoomLocatorContext>();
+                    var logger = scopedServices.GetRequiredService<ILogger<RoomLocatorApplicationFactory<TStartup>>>();
+
+                    db.Database.EnsureCreated();
+
+                    try
+                    {
+                        DatabaseSeedHelper.SeedRoles(db);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, $"As error occurred seeding the database with test data. Error: {ex.Message}");
+                    }
+                }
+            });
+        }
+    }
+}
