1389d897ac544ac9596000b76259ebfa9cde0cdf
algorythm <anders@wiberg.tech>
2019-11-22 16:31:54 +0100

    Add unauthorized exception to ExceptionFactory

--- RoomLocator/Shared/ExceptionFactory.cs
+++ RoomLocator/Shared/ExceptionFactory.cs
@@ -9,8 +9,13 @@ namespace Shared
         public static InvalidRequestException Create_FailedHttpRequest(string name)
         {
             return new InvalidRequestException("Failed to reach server", 
-            $"Failed to access {name}. Their servers might be down, please try again later.");
+                $"Failed to access {name}. Their servers might be down, please try again later.");
         }
-        
+
+        /// <summary>
+        /// Used when credentials are incorrect or a user is refused access to the site
+        /// </summary>
+        /// <returns></returns>
+        public static UnauthorizedException Unauthorized() => new UnauthorizedException();
     }
 }
